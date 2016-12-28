cf53bae2154d62eedfa4f8de58d726c4ba2885e2
Alex Muller <alex.muller@digital.cabinet-office.gov.uk>
2016-12-28 12:00:40 +0000

    Switch from using Jenkins to Travis
    Similar to the other frontend repos, this repo is going to publish
    with Travis now rather than using GOV.UK's Jenkins.
    
    I've added a deploy key to this repo on GitHub and the encrypted environment
    contains the RubyGems API key.
    
    I don't know why we were doing `rm Gemfile.lock` so I've removed it.

--- publish.sh
+++ publish.sh
@@ -18,17 +18,13 @@ git submodule foreach git pull origin master
 # The version of the toolkit defined in the submodules master branch
 NEW_SUBMODULE_VERSION=`cat app/assets/VERSION.txt`
 
-# Install gem dependencies, run tests, publish gem
-rm -f Gemfile.lock
-bundle install --path "${HOME}/bundles/${JOB_NAME}"
+# Install gem dependencies and run tests
+bundle install
 bundle exec rake
 
 # If the submodule has a new version string
 if [ "$PINNED_SUBMODULE_VERSION" != "$NEW_SUBMODULE_VERSION" ]; then
   # Commit the updated submodule and push it to origin
   git commit -am "Bump to version $NEW_SUBMODULE_VERSION"
-  git push origin master
+  git push origin_ssh master
 fi
-
-# Publish the new gem
-bundle exec rake publish_gem
